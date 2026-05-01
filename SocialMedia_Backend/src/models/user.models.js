b853460250e70ba25496028fd39c7ef7ec7ace6e
Pariskrit Bhagat <bhagataryan784@gmail.com>
2026-05-01 23:10:35 +0545

    Add methods for creating refresh token and access token

--- SocialMedia_Backend/src/models/user.models.js
+++ SocialMedia_Backend/src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     name: {
@@ -31,11 +32,11 @@ const userSchema = new Schema(
       enum: ["Male", "Female", "Others"],
       default: "Others",
     },
-    userProfile: {
+    userProfilePic: {
       type: String,
       default: "",
     },
-    userProfilePublicId: {
+    userProfilePicPublicId: {
       type: String,
     },
 
@@ -66,17 +67,45 @@ const userSchema = new Schema(
       enum: ["admin", "user"],
       default: "user",
     },
+    refreshToken:{
+        type:String,
+    }
   },
   { timestamps: true },
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
