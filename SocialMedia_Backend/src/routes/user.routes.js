cf2ea75bc695a6841cd7be38964e869663b1936b
Pariskrit Bhagat <bhagataryan784@gmail.com>
2026-05-04 10:50:45 +0545

    Created routes for changing user profile

--- SocialMedia_Backend/src/routes/user.routes.js
+++ SocialMedia_Backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.js";
-import { changeEmail, logoutUser, userLogin, userRegister } from "../controllers/users.controller.js";
+import { changeEmail, changeUserProfilePic, logoutUser, userLogin, userRegister } from "../controllers/users.controller.js";
 import {verifyJwt} from "../middlewares/auth.js"
 
 
@@ -10,4 +10,5 @@ router.route("/register").post(upload.single("userProfilePic"), userRegister);
 router.route("/login").post(userLogin)
 router.route("/logout").post(verifyJwt,logoutUser)
 router.route("/changeEmail").put(verifyJwt,changeEmail)
+router.route("/changeUserProfilePic").put(verifyJwt,upload.single("userProfilePic"),changeUserProfilePic);
 export default router;
