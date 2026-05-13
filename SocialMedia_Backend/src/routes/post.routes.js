b628755ccc73fe05158ac61799d0f363d2ca19d2
Pariskrit Bhagat <bhagataryan784@gmail.com>
2026-05-13 10:10:21 +0545

    add all the routes of post

--- SocialMedia_Backend/src/routes/post.routes.js
+++ SocialMedia_Backend/src/routes/post.routes.js
@@ -2,7 +2,17 @@ import { Router } from "express";
 
 import { verifyJwt } from "../middlewares/auth.js";
 import { upload } from "../middlewares/multer.js";
-import { createPost } from "../controllers/post.controllers.js";
+import {
+  createPost,
+  getAllPost,
+  getPostById,
+  getPostOfLoggedInUser,
+  getPostOfUserById,
+  deletePost,
+  likePost,
+  dislikePost,
+  updatePost,
+} from "../controllers/post.controllers.js";
 const router = Router();
 
 router
@@ -12,6 +22,16 @@ router
 router
   .route("/createPost/:communitieId")
   .post(verifyJwt, upload.array("media", 10), createPost);
+
+router.route("/getAllPost").get(getAllPost);
+router.route("/getPost/:postId").get(verifyJwt, getPostById);
+router.route("/getPostOfLoggedInUser").get(verifyJwt, getPostOfLoggedInUser);
+router.route("/getPostOfUser/:userId").get(verifyJwt, getPostOfUserById);
+router.route("/updatePost/:id").patch(verifyJwt, upload.array("media", 10), updatePost);
+router.route("/deletePost/:postId").delete(verifyJwt, deletePost);
+router.route("/like/:id").post(verifyJwt, likePost);
+router.route("/dislike/:id").post(verifyJwt, dislikePost);
+
 
 
 export default router;
