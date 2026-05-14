0e3ce05b1a155a7ae88c00e4b7a442aab27ef5ec
Pariskrit Bhagat <bhagataryan784@gmail.com>
2026-05-14 21:47:33 +0545

    created routes of community

--- SocialMedia_Backend/src/routes/community.route.js
+++ SocialMedia_Backend/src/routes/community.route.js
@@ -3,13 +3,13 @@ import { verifyJwt } from "../middlewares/auth.js";
 import { upload } from "../middlewares/multer.js";
 import {
   createCommunity,
-  updateCommunity,
+  // updateCommunity,
   deleteCommunity,
-  getCommunitiesOfLoggedInUser,
-  getCommunityById,
-  getAllCommunities,
-  searchCommunities,
-  getPersonalizedFeed,
+  // getCommunitiesOfLoggedInUser,
+  // getCommunityById,
+  // getAllCommunities,
+  // searchCommunities,
+  // getPersonalizedFeed,
 } from "../controllers/community.controllers.js";
 
 const router = Router();
@@ -19,13 +19,13 @@ const communityUpload = upload.fields([
   { name: "banner", maxCount: 1 },
 ]);
 
-router.route("/feed").get(verifyJwt, getPersonalizedFeed);
+// router.route("/feed").get(verifyJwt, getPersonalizedFeed);
 router.route("/createCommunity").post(verifyJwt, communityUpload, createCommunity);
-router.route("/updateCommunity/:communityId").patch(verifyJwt, communityUpload, updateCommunity);
+// router.route("/updateCommunity/:communityId").patch(verifyJwt, communityUpload, updateCommunity);
 router.route("/deleteCommunity/:communityId").delete(verifyJwt, deleteCommunity);
-router.route("/myCommunities").get(verifyJwt, getCommunitiesOfLoggedInUser);
-router.route("/search").get(verifyJwt, searchCommunities);
-router.route("/").get(verifyJwt, getAllCommunities);
-router.route("/:communityId").get(verifyJwt, getCommunityById);
+// router.route("/myCommunities").get(verifyJwt, getCommunitiesOfLoggedInUser);
+// router.route("/search").get(verifyJwt, searchCommunities);
+// router.route("/").get(verifyJwt, getAllCommunities);
+// router.route("/:communityId").get(verifyJwt, getCommunityById);
 
 export default router;
